b8fb74072c701527a7a228164adf5a1a71243e7b
Adam K <adam.kenney531@gmail.com>
2022-03-23 01:28:17 -0400

    Added More graphs and organization widgets and column dictionaries

---
+++ ColumnDictionary.py
@@ -0,0 +1,45 @@
+c_dict_pass = {
+    'Player': 'Player Name',
+    'Pass Yds': 'Total Passing Yards',
+    'Yds/Att': 'Average Yards Per Passing Attempt',
+    'Att': 'Passing Attempts',
+    'Cmp': 'Completed Passes',
+    'Cmp%': 'Completion Percentage',
+    'TD': 'Touchdown Passes',
+    'INT': 'Interceptions',
+    'Rate': 'Passer Rating',
+    '1st': 'First Down Passes',
+    '1st%': 'First Down Percentage',
+    '20+': 'Throws For 20+ Yards',
+    '40+': 'Throws For 40+ Yards',
+    'Lng': 'Longest Yards For A Single Pass',
+    'Sck': 'Passer Was Sacked',
+    'SckY': 'Yards Lost From Sacks'
+}
+c_dict_rush = {
+    'Player': 'Player Name',
+    'Rush Yds': 'Total Rushing Yards',
+    'Att': 'Rushing Attempts',
+    'TD': 'Rushing Touchdowns',
+    '20+': 'Number Of 20+ Yard Plays',
+    '40+': 'Number Of 40+ Yard Plays',
+    'Lng': 'Longest Run',
+    'Rush 1st': 'Rushing First Downs',
+    'Rush 1st%': 'Rushing First Down Percentage',
+    'Rush FUM': 'Rushing Fumbles'
+}
+
+c_dict_receiving = {
+    'Player': 'Player Name',
+    'Rec': 'Number Of Receptions',
+    'Yds': 'Number Of Receiving Yards',
+    'TD': 'Number Of Touchdowns',
+    '20+': '20+ Yard Plays',
+    '40+': '40+ Yard Plays',
+    'Lng': 'Most Yards In A Single Play',
+    'Rec 1st': 'First Down Catches',
+    '1st%': 'First Down Percentage',
+    'Rec FUM': 'Receiving Fumbles',
+    'Rec YAC/R': 'Yards After Catch',
+    'Tgts': 'Number Of Times Player Was The Targeted Receiver'
+}
--- main.py
+++ main.py
@@ -1,21 +1,15 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import enum
 from bs4 import BeautifulSoup
 import requests
 import matplotlib.pyplot as plt
 import seaborn as sns
+import ColumnDictionary
 
 # https://www.nfl.com/stats/player-stats/category/passing/2021/REG/all/passingyards/desc
 # https://www.nfl.com/stats/player-stats/category/rushing/2021/REG/all/rushingyards/desc
 # https://www.nfl.com/stats/player-stats/category/receiving/2019/REG/all/receivingreceptions/desc
-
-
-class POSITIONS(enum.Enum):
-    qb = "passing"
-    running = "rushing"
-    receiver = "receiving"
 
 
 @st.cache(show_spinner=False)
@@ -28,7 +22,7 @@ def load_data(url):
     df = html[0]
 
     while True:
-        soup = BeautifulSoup(page.content)
+        soup = BeautifulSoup(page.content, features="lxml")
         pag = soup.find_all(class_='nfl-o-table-pagination__buttons')
         if len(pag) == 0:
             break
@@ -37,7 +31,7 @@ def load_data(url):
             url = 'https://www.nfl.com' + a['href']
             page = requests.get(url, headers=headers)
             html = pd.read_html(page.text, header=0)
-            df = df.append(html[0])
+            df = pd.concat([df, html[0]])
 
     return df
 
@@ -58,7 +52,7 @@ def make_url(p, y):
 
 # MAIN
 st.title('NFL Offensive Football Player Stats')
-st.sidebar.subheader("Input Options")
+st.sidebar.subheader("Input Options:")
 pos = st.sidebar.selectbox('Select Position', ["passing", "rushing", "receiving"])
 year = st.sidebar.selectbox('Select Year', [*range(2021, 1969, -1)])
 
@@ -67,28 +61,79 @@ u = make_url(pos, year)
 with st.spinner(f"Loading data for {pos}, {year}"):
     data = load_data(u)
 
-st.sidebar.subheader("Filter Options")
-columns = st.sidebar.multiselect("Columns to include", list(data.columns), default=list(data.columns))
-if 'All' not in columns and len(columns) != 0:
-    data = data[columns]
+st.sidebar.subheader("Filter Options:")
+if st.sidebar.checkbox("Filter columns"):
+    columns = st.sidebar.multiselect("Columns to include", list(data.columns), default=list(data.columns))
+    if len(columns) != 0:
+        data = data[columns]
 
-stat_options = ['None'] + list(data.columns)
-if 'Player' in stat_options:
-    stat_options.remove('Player')
+if st.sidebar.checkbox("Filter by percentile"):
+    stat_options = ['None'] + list(data.columns)
+    if 'Player' in stat_options:
+        stat_options.remove('Player')
 
-stat = st.sidebar.selectbox("Statistic For Percentile", stat_options)
-perc = st.sidebar.slider("Choose Percentile", min_value=10, max_value=100, step=5)
-area = st.sidebar.selectbox(f"Players ____ the {perc}th percentile in {stat}", ['Above', 'Below'])
-if stat != 'None':
-    if area == 'Above':
-        data = data.loc[data[stat] > np.percentile(data[stat], perc)]
+    stat = st.sidebar.selectbox("Statistic For Percentile", stat_options)
+    perc = st.sidebar.slider("Choose Percentile", min_value=10, max_value=100, step=5)
+    area = st.sidebar.selectbox(f"Players ____ the {perc}th percentile in {stat}", ['Above', 'Below'])
+    if stat != 'None':
+        if area == 'Above':
+            data = data.loc[data[stat] > np.percentile(data[stat], perc)]
+        else:
+            data = data.loc[data[stat] < np.percentile(data[stat], perc)]
+
+if st.checkbox("Show Column Dictionary"):
+    if pos == 'passing':
+        c_dict = ColumnDictionary.c_dict_pass
+    elif pos == 'receiving':
+        c_dict = ColumnDictionary.c_dict_receiving
     else:
-        data = data.loc[data[stat] < np.percentile(data[stat], perc)]
+        c_dict = ColumnDictionary.c_dict_rush
+
+    d = pd.DataFrame({'Column Name': c_dict.keys(), 'Description': c_dict.values()})
+    st.write(d)
 
 if st.checkbox("Show Data", value=True):
     st.subheader(f"{str.upper(pos)}, {year}")
     st.text(f"Data is {data.shape[0]} x {data.shape[1]}")
     st.write(data)
+
+if st.checkbox("Show Aggregated Data"):
+    c = list(data.columns)
+    if 'Player' in c:
+        c.remove('Player')
+    agg_data = data[c].agg(func=['mean', 'std', 'var', 'sem', 'min', 'max']).round(2)
+    st.write(agg_data)
+
+if st.checkbox("Show Histogram"):
+    col_options = list(data.columns)
+    if 'Player' in col_options:
+        col_options.remove('Player')
+    column = st.selectbox("Select Statistic For Histogram", col_options)
+    n_bins = st.slider("Number of bins", min_value=5, max_value=40)
+    bins = list(range(0, data[column].max(), int(data[column].max() / n_bins)))
+    plt.figure(figsize=(8, 4))
+    plt.hist(data[column], bins=bins)
+    # Change x-ticks based on n_bins so that they do not overlap
+    if n_bins >= 30:
+        plt.xticks(bins[::4])
+    elif n_bins >= 15:
+        plt.xticks(bins[::2])
+    else:
+        plt.xticks(bins)
+
+    plt.ylabel(column)
+    plt.xlabel('Num Players')
+    st.pyplot(plt)
+
+if st.checkbox("Show Box Plot"):
+    col_options = list(data.columns)
+    if 'Player' in col_options:
+        col_options.remove('Player')
+    column = st.selectbox("Select Statistic For Box Plot", col_options)
+    plt.figure(figsize=(4, 6))
+    plt.boxplot(data[column], labels=[f'{column} among players'])
+    plt.ylabel(column)
+    st.pyplot(plt)
 
 if st.checkbox("Show Heat Map"):
     corr = data.corr()
@@ -96,6 +141,7 @@ if st.checkbox("Show Heat Map"):
     # Upper Triangle
     mask[np.triu_indices_from(mask)] = 1
     with sns.axes_style("white"):
-        f, ax = plt.subplots(figsize=(7, 5))
-        ax = sns.heatmap(corr, mask=mask, vmax=1, square=True)
-        st.pyplot(f)
+        f_heat, ax_heat = plt.subplots(figsize=(7, 5))
+        ax_heat = sns.heatmap(corr, mask=mask, vmax=1, square=True)
+        st.pyplot(f_heat)
+
